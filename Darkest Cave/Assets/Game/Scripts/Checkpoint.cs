89734ba48ae4e2684ca3705e34f9a23206d96ec1
KevinGacon <48351155+KevinGacon@users.noreply.github.com>
2021-01-14 15:32:46 +0100

    banc (restore mana + health)

--- Darkest Cave/Assets/Game/Scripts/Checkpoint.cs
+++ Darkest Cave/Assets/Game/Scripts/Checkpoint.cs
@@ -14,6 +14,21 @@ public class Checkpoint : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             playerSpawn.position = transform.position;
+
+            PlayerHealth.instance.currentHealth = PlayerHealth.instance.maxHealth;
+            PlayerHealth.instance.healthbar.SetHealth(PlayerHealth.instance.currentHealth);
+            PlayerMana.instance.currentMana = PlayerMana.instance.maxMana;
+            PlayerMana.instance.currentManaDisplay.text = PlayerMana.instance.currentMana.ToString();
+
+            SaveData();
         }
+    }
+
+    private void SaveData()
+    {
+        PlayerPrefs.SetInt("Coins", Inventory.instance.coinsCount);
+
+        PlayerPrefs.SetFloat("PositionX", playerSpawn.position.x);
+        PlayerPrefs.SetFloat("PositionY", playerSpawn.position.y);
     }
 }
